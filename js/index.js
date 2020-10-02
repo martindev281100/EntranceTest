fde8566582473491deb6230d943f1a8e6f89dd3a
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-10-02 10:35:23 +0700

    update commit

--- js/index.js
+++ js/index.js
@@ -1,92 +1,92 @@
-// function mergeArr() {
-//     let newArr = []
-//     let A1 = prompt('Enter Array 1 (With commas): ');
-//     A1 = A1.split(',')
-//     let A2 = prompt('Enter Array 2 (With commas): ');
-//     A2 = A2.split(',')
-//     for (let i = 0; i < A1.length; i++) {
-//         A1[i] = Number(A1[i])
-//     }
-//     for (let i = 0; i < A2.length; i++) {
-//         A2[i] = Number(A2[i])
-//     }
-//     console.log(A1)
-//     console.log(A2)
-
-//     for (let i = 0; i < A1.length; i++) {
-//         if (newArr.indexOf(A1[i]) === -1) {
-//             newArr.push(A1[i])
-//         }
-//     }
-//     for (let i = 0; i < A2.length; i++) {
-//         if (newArr.indexOf(A2[i]) === -1) {
-//             newArr.push(A2[i])
-//         }
-//     }
-//     console.log(newArr)
-// }
-// mergeArr()
+// Bai 1. A
+function mergeArr() {
+    let newArr = []
+    let A1 = prompt('Enter Array 1 (With commas): ');
+    A1 = A1.split(',')
+    let A2 = prompt('Enter Array 2 (With commas): ');
+    A2 = A2.split(',')
+    for (let i = 0; i < A1.length; i++) {
+        A1[i] = Number(A1[i])
+    }
+    for (let i = 0; i < A2.length; i++) {
+        A2[i] = Number(A2[i])
+    }
 
 
-// function ranking() {
-//     let data = [{
-//         name: "Arsenal",
-//         points: 60,
-//         GD: 40
-//     }, {
-//         name: "Chelsea",
-//         points: 75,
-//         GD: 45
-//     }, {
-//         name: "Manchester City",
-//         points: 72,
-//         GD: 50
-//     }, {
-//         name: "Manchester United",
-//         points: 80,
-//         GD: 60
-//     }, {
-//         name: "Liverpool",
-//         points: 55,
-//         GD: 30
-//     }, {
-//         name: "Tottenham",
-//         points: 55,
-//         GD: 33
-//     }, {
-//         name: "Leicester City",
-//         points: 55,
-//         GD: 33
-//     }]
+    for (let i = 0; i < A1.length; i++) {
+        if (newArr.indexOf(A1[i]) === -1) {
+            newArr.push(A1[i])
+        }
+    }
+    for (let i = 0; i < A2.length; i++) {
+        if (newArr.indexOf(A2[i]) === -1) {
+            newArr.push(A2[i])
+        }
+    }
+    console.log('Bai1.a:'+ newArr)
+}
+mergeArr()
 
-//     for (let i = 0; i < data.length; i++) {
-//         for (let i = 1; i < data.length; i++) {
-//             console.log(data[i - 1].points + ' < ' + data[i].points)
-//             if (data[i - 1].points < data[i].points) {
-//                 let a = data[i - 1]
-//                 data[i - 1] = data[i]
-//                 data[i] = a
-//             } else if (data[i - 1].points === data[i].points) {
-//                 if (data[i - 1].GD < data[i].GD) {
-//                     let b = data[i - 1]
-//                     data[i - 1] = data[i]
-//                     data[i] = b
-//                 } else if (data[i - 1].GD === data[i].GD) {
-//                     if (data[i - 1].name[0] > data[i].name[0]) {
-//                         let c = data[i - 1]
-//                         data[i - 1] = data[i]
-//                         data[i] = c
-//                     }
-//                 }
-//             }
-//         }
-//     }
+//Bai 1.B: 
+function ranking() {
+    let data = [{
+        name: "Arsenal",
+        points: 60,
+        GD: 40
+    }, {
+        name: "Chelsea",
+        points: 75,
+        GD: 45
+    }, {
+        name: "Manchester City",
+        points: 72,
+        GD: 50
+    }, {
+        name: "Manchester United",
+        points: 80,
+        GD: 60
+    }, {
+        name: "Liverpool",
+        points: 55,
+        GD: 30
+    }, {
+        name: "Tottenham",
+        points: 55,
+        GD: 33
+    }, {
+        name: "Leicester City",
+        points: 55,
+        GD: 33
+    }]
 
-//     console.log(data)
-// }
+    for (let i = 0; i < data.length; i++) {
+        for (let i = 1; i < data.length; i++) {
+            if (data[i - 1].points < data[i].points) {
+                let a = data[i - 1]
+                data[i - 1] = data[i]
+                data[i] = a
+            } else if (data[i - 1].points === data[i].points) {
+                if (data[i - 1].GD < data[i].GD) {
+                    let b = data[i - 1]
+                    data[i - 1] = data[i]
+                    data[i] = b
+                } else if (data[i - 1].GD === data[i].GD) {
+                    if (data[i - 1].name[0] > data[i].name[0]) {
+                        let c = data[i - 1]
+                        data[i - 1] = data[i]
+                        data[i] = c
+                    }
+                }
+            }
+        }
+    }
 
-// ranking();
+    console.log('Bai1.b:' +data)
+}
 
+ranking();
+
+//Bai 2:
 async function quizApi() {
     document.getElementById("app").innerHTML = component.quizPage
 
